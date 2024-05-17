2e43f1d1b88855cf1ccc20019026137b9806a6fa
Minsub Kim <rapidfsub@gmail.com>
2024-05-17 18:42:01 +0900

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,16 @@
+set PATH /usr/local/bin:$PATH
+set -l __PATH__ $PATH
+### https://github.com/jetify-com/devbox/issues/1744#issuecomment-2071809825
+devbox global shellenv --init-hook --recompute | sed -e 's/; then/;/' | sed -e 's/^fi/end/' | sed -e 's/hash -r//' | source
+# devbox global shellenv --init-hook | source
+set PATH __PATH__:$PATH
+
+if test "$(arch)" = arm64
+    /opt/homebrew/bin/brew shellenv | source
+else
+    /usr/local/bin/brew shellenv | source
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias asdf=mise
@@ -10,18 +23,6 @@ if status is-interactive
     alias smd="export MIX_ENV=dev"
     alias smt="export MIX_ENV=test"
     alias tl=tldr
-end
-
-set -l __PATH__ $PATH
-### https://github.com/jetify-com/devbox/issues/1744#issuecomment-2071809825
-devbox global shellenv --init-hook --recompute | sed -e 's/; then/;/' | sed -e 's/^fi/end/' | sed -e 's/hash -r//' | source
-# devbox global shellenv --init-hook | source
-set PATH __PATH__:$PATH
-
-if test "$(arch)" = arm64
-    /opt/homebrew/bin/brew shellenv | source
-else
-    /usr/local/bin/brew shellenv | source
 end
 
 direnv hook fish | source
